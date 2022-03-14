72579383771f53d14d45ede90894caeceb9774ec
Sanjeev Bharati <mesanjeev617@gmail.com>
2022-03-14 14:02:14 -0600

    Starting

---
+++ BookESale/Models/Category.cs
@@ -0,0 +1,17 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+//means inside BookEsale under Models folder...
+namespace BookESale.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public int DisplayOrder { get; set; }
+
+        public DateTime dateTime { get; set; } = DateTime.Now;
+
+    }
+}
